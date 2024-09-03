da5fb6c1005ada5a34d934131f583bee5c8fd6bf
Mateusz Fryc <mateuszfryc@gmail.com>
2024-09-03 17:56:39 +0200

    feat: whisper script arguments

--- Whisper/app/run.py
+++ Whisper/app/run.py
@@ -1,3 +1,4 @@
+import argparse
 import logging
 import threading
 import time
@@ -14,12 +15,28 @@ logger.setLevel(logging.INFO)
 # ! use multiprocessing instead of threading to avoid Global Interpreter Lock (GIL)
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="A script to handle specific CLI arguments.")
+
+    # Add arguments
+    parser.add_argument('-l', '--language', type=str, default="en", help='Set the initial transcription language (default: "en")')
+    parser.add_argument('-m', '--multilingual', action='store_true', help='Enable multilingual support')
+    parser.add_argument('-d', '--debug', action='store_true', help='Enable debug mode')
+    parser.add_argument('-p', '--print_in_loop', action='store_true', help='Enable printing in a loop')
+
+    # Parse the arguments
+    args = parser.parse_args()
+
+    print(f"Initial language: {args.language}")
+    print(f"Multilingual: {args.multilingual}")
+    print(f"Debug: {args.debug}")
+    print(f"Print in Loop: {args.print_in_loop}")
+
     server = TranscriptionServer()
     transcription = TranscriptionService(
-        language="pl",
-        multilingual=True,
-        debug=True,
-        print_in_loop=True
+        language=args.language,
+        multilingual=args.multilingual,
+        debug=args.debug,
+        print_in_loop=args.print_in_loop
     )
 
     def on_start_capture(data):
@@ -42,7 +59,13 @@ if __name__ == "__main__":
         transcription.pause()
         return True
 
+    def on_silence_detected(current_transcript_text):
+        info(f'Silence detected. Pausing transcription. Current transcript: {current_transcript_text}')
+        server.receive_audio = False
+        transcription.pause()
+
     transcription.add_event_handler(TranscriptEvent.REPEATED_THRESHOLD, on_final_transcript)
+    transcription.add_event_handler(TranscriptEvent.SILENCE_AFTER, on_silence_detected)
 
     try:
         transcription_thread = threading.Thread(target=transcription.transcription_loop, daemon=True)
