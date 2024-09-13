0a8c965b3516bec7ad8f987b6670c786b9ec47f5
Mateusz Fryc <mateuszfryc@gmail.com>
2024-09-13 12:35:14 +0200

    wip: transcription service

--- Whisper/app/run.py
+++ Whisper/app/run.py
@@ -5,7 +5,8 @@ import time
 
 from custom_logger import error, info, warn
 from transcription_server import ServerEvent, TranscriptionServer
-from transcription_service import TranscriptEvent, TranscriptionService
+# from transcription_service import TranscriptEvent, TranscriptionService
+from transcription_service import TranscriptionService
 
 logger = logging.getLogger()
 # logger.setLevel(logging.WARNING)
@@ -13,6 +14,33 @@ logger.setLevel(logging.INFO)
 # logger.setLevel(logging.DEBUG)
 
 # ! use multiprocessing instead of threading to avoid Global Interpreter Lock (GIL)
+
+excluded_phrases = [
+    "PARROT TV",
+    "Dziękuje za obejrzenie",
+    "Thank you for your support on Patronite.",
+    "Zapraszam na kolejny odcinek!",
+    "Napisy stworzone przez społeczność Amara.org"
+    "Amara.org",
+    "Dzięki za uwagę",
+    "Dzięki za obejrzenie!",
+    "Transkrypcja Magdalena Świerczek-Gryboś",
+    "Transkrypcja Magdalena Świerczek-Gryboś © PTA – TVP",\
+    "PTA – TVP",
+    "Dziękuję za uwagę",
+    "Dziękuję za uwagę i do zobaczenia w kolejnym odcinku.",
+    "Polskie Towarzystwo Astronomiczne",
+    "Produkcja Polskie Towarzystwo Astronomiczne Telewizja Polska",
+    "zapomnijcie zasubskrybować",
+    "zafollowować",
+    "Nie zapomnijcie zasubskrybować oraz zafollowować mnie na Facebooku!",
+    "Zdjęcia i montaż",
+    "Pracownia Prawa i Sprawiedliwość",
+    "Teraz wracamy do naszego odcinka"
+    "Teraz wracamy do naszego odcinka, w którym pokażę Wam",
+    "najnowszych filmików na świecie",
+    "Dziękuje za oglądanie i do zobaczenia w kolejnych odcinkach!"
+]
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="A script to handle specific CLI arguments.")
@@ -33,21 +61,23 @@ if __name__ == "__main__":
 
     server = TranscriptionServer()
     transcription = TranscriptionService(
+        device_index=1,
         language=args.language,
-        multilingual=args.multilingual,
+        # multilingual=args.multilingual,
         debug=args.debug,
-        print_in_loop=args.print_in_loop
+        # print_in_loop=args.print_in_loop,
+        excluded_phrases=excluded_phrases
     )
 
     def on_start_capture(data):
         if "language" in data:
             transcription.set_language(data["language"])
-        transcription.reset_state()
-        transcription.resume()
-        server.receive_audio = True
+        # transcription.reset_state()
+        # transcription.resume()
+        server.set_receive_audio(True)
 
     def on_stop_capture():
-        server.receive_audio = False
+        server.set_receive_audio(False)
 
     server.add_event_handler(ServerEvent.START_CAPTURE, on_start_capture)
     server.add_event_handler(ServerEvent.STOP_CAPTURE, on_stop_capture)
@@ -56,41 +86,43 @@ if __name__ == "__main__":
     def on_final_transcript(transcript):
         info(f"Final transcript: {transcript}")
         server.send(f"[FINAL]{transcript}")
-        transcription.pause()
-        return True
+        # transcription.pause()
+        # return True
 
-    def on_silence_detected(current_transcript_text):
-        info(f'Silence detected. Pausing transcription. Current transcript: {current_transcript_text}')
-        server.receive_audio = False
-        transcription.pause()
-        return True
+    # def on_silence_detected(current_transcript_text):
+    #     info(f'Silence detected. Pausing transcription. Current transcript: {current_transcript_text}')
+    #     server.receive_audio = False
+    #     # transcription.pause()
+    #     return True
 
-    transcription.add_event_handler(TranscriptEvent.REPEATED_THRESHOLD, on_final_transcript)
-    transcription.add_event_handler(TranscriptEvent.SILENCE_AFTER, on_silence_detected)
+    transcription.set_on_transcription_callback(on_final_transcript)
+    # transcription.add_event_handler(TranscriptEvent.REPEATED_THRESHOLD, on_final_transcript)
+    # transcription.add_event_handler(TranscriptEvent.SILENCE_AFTER, on_silence_detected)
 
-    try:
-        transcription_thread = threading.Thread(target=transcription.transcription_loop, daemon=True)
-    except Exception as e:
-        error(f"Could not initiate transcription thread: {e}")
+    # try:
+    #     transcription_thread = threading.Thread(target=transcription.transcription_loop, daemon=True)
+    # except Exception as e:
+    #     error(f"Could not initiate transcription thread: {e}")
 
     try:
         server_thread = threading.Thread(target=server.server.serve_forever, daemon=True)
     except Exception as e:
         error(f"Could not initiate websocket server thread: {e}")
 
-    transcription_thread.start()
+    # transcription_thread.start()
     info(f"Started transcription. {transcription.get_status_message()}")
 
     server_thread.start()
     info(f"Started websocket server on {server.host}:{server.port}")
 
-    transcription.pause()
-    transcription.reset_state()
+    # transcription.pause()
+    # transcription.reset_state()
 
     def end():
         server.server.shutdown()
         # server.server.server_close()
-        transcription.cleanup()
+        transcription.stop()
+        # transcription.cleanup()
         info("Quitting application...")
         quit()
 
